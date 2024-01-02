dfbf16a7b86e8e39f050d7cbabbbcc6b9fcec600
Muhammad Awais <118798522+mhmd11Awais@users.noreply.github.com>
2024-01-02 10:29:29 +0500

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(mongodb+srv://200612:12345@cluster0.9d1ek26.mongodb.net/);
     console.log(`Database Connected: `);
   } catch (error) {
     console.log(error);
